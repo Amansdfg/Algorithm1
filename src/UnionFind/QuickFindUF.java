c5e1461bd1cc635bad0c3cae7fd22957e7c3a563
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2024-09-12 15:51:12 +0500

    Quick Find

--- src/UnionFind/QuickFindUF.java
+++ src/UnionFind/QuickFindUF.java
@@ -1,2 +1,28 @@
-package UnionFind;public class QuickFindUF {
+package UnionFind;
+
+public class QuickFindUF {
+    private  int[] id;
+    public QuickFindUF(int N) {
+        id=new int[N];
+        for(int i=0;i<N;i++){
+            id[i]=i;
+        }
+    }
+    public boolean connected(int p,int q){
+        return id[p]==id[q];
+    }
+    public void union(int p,int q){
+        int pId=id[p];
+        int qId=id[q];
+        for(int i=0;i<id.length;i++){
+            if(id[i]==pId){
+                id[i]=qId;
+            }
+        }
+    }
+    public void print(){
+        for(int i=0;i<id.length;i++){
+            System.out.println(i+" "+id[i]);
+        }
+    }
 }
--- src/UnionFind/Test.java
+++ src/UnionFind/Test.java
@@ -1,2 +1,20 @@
-package UnionFind;public class Test {
+package UnionFind;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        QuickFindUF uf = new QuickFindUF(10);
+        for (int i = 0; i < 10; i++) {
+            int p= sc.nextInt();
+            int q= sc.nextInt();
+            if(!uf.connected(p,q)){
+                uf.union(p,q);
+                System.out.println(p+" "+q);
+            }
+        }
+        System.out.println("Finish");
+        uf.print();
+    }
 }
